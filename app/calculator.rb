e5b4ad8bd48c1cb993113772e4cc2f2a5e17709e
Amit Natani <eng.amitnatani1993@gmail.com>
2024-12-29 22:25:17 +0530

    Raising expcetion from add method for string with negative numbers

--- app/calculator.rb
+++ app/calculator.rb
@@ -2,16 +2,22 @@ class Calculator
   def add(numbers)
     return 0 if numbers.nil? || numbers.empty?
 
+    negatives = []
     numbers, delimiter = find_delimiter(numbers)
     sum = 0
     numbers.split(delimiter).each do |number|
       if number.include?("\n")
         number.split("\n").each do |num|
+          check_negative_number(number, negatives)
           sum += num.to_i
         end
       else
+        check_negative_number(number, negatives)
         sum += number.to_i
       end
+    end
+    if negatives.length > 0
+      raise ArgumentError.new("negative numbers not allowed #{negatives.join(",")}")
     end
     sum
   end
@@ -23,5 +29,11 @@ class Calculator
       numbers = numbers[3..]
     end
     [numbers, delimiter]
+  end
+
+  def check_negative_number(number, negatives)
+    return if number.to_i >= 0
+
+    negatives << number.to_i
   end
 end
