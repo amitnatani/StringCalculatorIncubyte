ec33d4d7fff50a2458f0b5aac1a50e473af219a2
Amit Natani <eng.amitnatani1993@gmail.com>
2024-12-29 22:03:24 +0530

    Added test case for string with comma delimiter

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -10,4 +10,9 @@ class CalculatorTest < Test::Unit::TestCase
     assert_equal 0, @calculator.add('')
     assert_equal 0, @calculator.add(nil)
   end
+
+  def test_add_with_comma_delimeter
+    assert_equal 6, @calculator.add('1,2,3')
+    assert_equal 9, @calculator.add('4, 2, 3')
+  end
 end
