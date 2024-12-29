1260ead74eddbf5c36bcabfedf794cd02d52891c
Amit Natani <eng.amitnatani1993@gmail.com>
2024-12-29 22:05:30 +0530

    Handle add method for strings with comma delimiter

--- app/calculator.rb
+++ app/calculator.rb
@@ -1,5 +1,11 @@
 class Calculator
   def add(numbers)
     return 0 if numbers.nil? || numbers.empty?
+
+    sum = 0
+    numbers.split(",").each do |number|
+      sum += number.to_i
+    end
+    sum
   end
 end
